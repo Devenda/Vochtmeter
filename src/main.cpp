503bf529713ff9441f32b361b8c80515a11ed182
Your Name <tinus.scheppers@telenet.be>
2018-07-20 16:06:02 +0200

    Added Example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
+#include "DHT.h"
+#include "cred.h"
 
-void setup() {
-    // put your setup code here, to run once:
+DHT dht11;
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println();
+    Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)");
+    Serial.println("SSID:");
+    Serial.println(SSID);
+
+    dht11.setup(2); // data pin 2
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+void loop()
+{
+    delay(dht11.getMinimumSamplingPeriod());
+
+    float humidity = dht11.getHumidity();
+    float temperature = dht11.getTemperature();
+
+    Serial.print(dht11.getStatusString());
+    Serial.print("\t");
+    Serial.print(humidity, 1);
+    Serial.print("\t\t");
+    Serial.print(temperature, 1);
+    Serial.print("\t\t");
+    Serial.println(dht11.toFahrenheit(temperature), 1);
 }
